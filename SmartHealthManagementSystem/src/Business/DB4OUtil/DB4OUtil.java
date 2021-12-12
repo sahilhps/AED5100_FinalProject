69c3a975672a968469a9e2f08f4fa676730094d4
vanditko <kothari.vandit12345@gmail.com>
2021-12-11 19:22:12 -0500

    DB4O storage error removed

--- SmartHealthManagementSystem/src/Business/DB4OUtil/DB4OUtil.java
+++ SmartHealthManagementSystem/src/Business/DB4OUtil/DB4OUtil.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package Business.DB4OUtil;
 
 import Business.ConfigureASystem;
@@ -11,12 +16,11 @@ import java.nio.file.Paths;
 
 /**
  *
- * @author rrheg
- * @author Lingfeng
+ * @author dongyueli
  */
 public class DB4OUtil {
-
-    private static final String FILENAME = Paths.get("Databank.db4o").toAbsolutePath().toString();// path to the data store
+    private static final String FILENAME = Paths.get("Databank.db4o").toAbsolutePath().toString(); // path to the data store
+    //"/Users/dongyueli/NetBeansProjects/lab/DataBank.db4o"
     private static DB4OUtil dB4OUtil;
     
     public synchronized static DB4OUtil getInstance(){
@@ -35,7 +39,17 @@ public class DB4OUtil {
     private ObjectContainer createConnection() {
         try {
 
-            ObjectContainer db = Db4oEmbedded.openFile( FILENAME);
+            EmbeddedConfiguration config = Db4oEmbedded.newConfiguration();
+            config.common().add(new TransparentPersistenceSupport());
+            //Controls the number of objects in memory
+            config.common().activationDepth(Integer.MAX_VALUE);
+            //Controls the depth/level of updation of Object
+            config.common().updateDepth(Integer.MAX_VALUE);
+
+            //Register your top most Class here
+            config.common().objectClass(EcoSystem.class).cascadeOnUpdate(true); // Change to the object you want to save
+
+            ObjectContainer db = Db4oEmbedded.openFile(config, FILENAME);
             return db;
         } catch (Exception ex) {
             System.out.print(ex.getMessage());
@@ -55,7 +69,7 @@ public class DB4OUtil {
         ObjectSet<EcoSystem> systems = conn.query(EcoSystem.class); // Change to the object you want to save
         EcoSystem system;
         if (systems.size() == 0){
-            system = ConfigureASystem.configure();  // If there's no System in the record, create a new one
+            system = ConfigureASystem.configureABusiness();  // If there's no System in the record, create a new one
         }
         else{
             system = systems.get(systems.size() - 1);
